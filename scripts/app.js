bdc66208a6263eccfc19b13ac1d4046982c9df3e
ismailkolhoze <ismailkolhoze@gmail.com>
2025-09-22 16:49:40 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 /*********************
  * SUPABASE BAŞLAT
  *********************/
-const SUPABASE_URL = 'https://vsekmddgqcnufzgskbeh.supabase.co;
+const SUPABASE_URL = 'https://vsekmddgqcnufzgskbeh.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzZWttZGRncWNudWZ6Z3NrYmVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxOTgwMjYsImV4cCI6MjA3Mzc3NDAyNn0.-CG-lckjDFs0PHSkwk7aD_t5zzHGAyytRKUwOskIjzQ';
 
 const { createClient } = window.supabase; // UMD için şart
